e3901b23a2137c78d37a571bfb599d85247046c2
Destiny Boone <destaj.db@gmail.com>
2023-04-11 12:24:07 -0400

    adding state slices

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,27 @@
 import { createStore } from 'redux';
+import { createSlice } from '@reduxjs/toolkit';
+
+createSlice({
+    name: 'counter',
+    initialState: { counter: 0, showCounter: true },
+    reducers: {
+        increment(state) {
+            state.counter++; // this is allowed when using redux toolkit bc rtoolkit translates it as 'copy old state and adjust this one specific value'
+        },
+        decrement(state) {
+            state.counter--;
+        },
+        increase(state, action) { // only have to accept the action as a parameter where it's needed
+            state.counter = state.counter + action.amount;
+        },
+        toggleCounter(state) {
+            state.showCounter = !state.showCounter;
+        },
+    }
+});
 
 const counterReducer = (state = { counter: 0, showCounter: true }, action) => {
+    // YOU MUST ALWAYS RETURN A NEW STATE RATHER THAN MUTATE THE EXISTING STATE.
     if(action.type === 'increment'){
         return{
             counter: state.counter + 1,
